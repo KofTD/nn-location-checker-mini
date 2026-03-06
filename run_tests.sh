b00d8ac9468a63263b1bba3a91fbc06c2fccbdd5
Daniel Nesterov <KOfTD@yandex.ru>
2026-03-06 09:27:31 +0300

    Switching to pytest

--- run_tests.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-source ./.venv/bin/activate
-python -m unittest ./tests/*.py
-deactivate
--- tests/convolution_test.py
+++ tests/convolution_test.py
@@ -1,19 +1,21 @@
-import unittest
+import sys
+
+src_directory = "../src/"
+
+sys.path.append(src_directory)
 
 import torch.nn as tnn
 
-from convolution import _compute_conv  # pyright: ignore[reportPrivateUsage]
-from convolution import _compute_shape  # pyright: ignore[reportPrivateUsage]
-from convolution import TensorShape
+from utils import TensorShape, compute_conv, compute_shape
 
 
-class ComputeConvTests(unittest.TestCase):
+class TestComputeConv:
     def test_conv(self):
-        result = _compute_conv(227, 2, 11, 4)
-        self.assertEqual(result, 56)
+        result = compute_conv(227, 2, 11, 4)
+        assert result == 56
 
 
-class ComputShapeTests(unittest.TestCase):
+class TestComputShape:
     def test_conv(self):
         shape = TensorShape(32, 32, 3)
         padding = (2, 2)
@@ -22,41 +24,41 @@ class ComputShapeTests(unittest.TestCase):
         conv = tnn.Conv2d(
             3, 64, padding=padding, kernel_size=kernel_size, stride=stride
         )
-        result = _compute_shape(conv, shape)
+        result = compute_shape(conv, shape)
 
-        checker_height = _compute_conv(
+        checker_height = compute_conv(
             shape.height, padding[0], kernel_size[0], stride[0]
         )
-        checker_width = _compute_conv(
+        checker_width = compute_conv(
             shape.width, padding[1], kernel_size[1], stride[1]
         )
         checker = TensorShape(checker_height, checker_width, 64)
 
-        self.assertEqual(result, checker)
+        assert result == checker
 
     def test_relu(self):
         activation = tnn.ReLU()
         shape = TensorShape(32, 32, 3)
 
-        result = _compute_shape(activation, shape)
+        result = compute_shape(activation, shape)
 
-        self.assertEqual(result, shape)
+        assert result == shape
 
     def test_dropout(self):
         activation = tnn.Dropout(0.5)
         shape = TensorShape(32, 32, 3)
 
-        result = _compute_shape(activation, shape)
+        result = compute_shape(activation, shape)
 
-        self.assertEqual(result, shape)
+        assert result == shape
 
     def test_batch_norm(self):
         activation = tnn.BatchNorm2d(3024)
         shape = TensorShape(32, 32, 3)
 
-        result = _compute_shape(activation, shape)
+        result = compute_shape(activation, shape)
 
-        self.assertEqual(result, shape)
+        assert result == shape
 
     def test_max_pool(self):
         shape = TensorShape(32, 32, 3)
@@ -64,38 +66,34 @@ class ComputShapeTests(unittest.TestCase):
         kernel_size = (5, 5)
         stride = (3, 3)
         pool = tnn.MaxPool2d(kernel_size[0], stride[0], padding[0])
-        result = _compute_shape(pool, shape)
+        result = compute_shape(pool, shape)
 
-        checker_height = _compute_conv(
+        checker_height = compute_conv(
             shape.height, padding[0], kernel_size[0], stride[0]
         )
-        checker_width = _compute_conv(
+        checker_width = compute_conv(
             shape.width, padding[1], kernel_size[1], stride[1]
         )
         checker = TensorShape(checker_height, checker_width, 3)
 
-        self.assertEqual(result, checker)
+        assert result == checker
 
     def test_adaptive_max_pool(self):
         shape = TensorShape(32, 32, 3)
         out_size = (3, 3)
         pool = tnn.AdaptiveMaxPool2d(out_size)
-        result = _compute_shape(pool, shape)
+        result = compute_shape(pool, shape)
 
         checker = TensorShape(out_size[0], out_size[1], 3)
 
-        self.assertEqual(result, checker)
+        assert result == checker
 
     def test_adaptive_avg_pool(self):
         shape = TensorShape(32, 32, 3)
         out_size = (3, 3)
         pool = tnn.AdaptiveAvgPool2d(out_size)
-        result = _compute_shape(pool, shape)
+        result = compute_shape(pool, shape)
 
         checker = TensorShape(out_size[0], out_size[1], 3)
 
-        self.assertEqual(result, checker)
-
-
-if __name__ == "__main__":
-    _ = unittest.main()
+        assert result == checker
