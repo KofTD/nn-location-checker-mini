2a36dec3a4bb1488d1e6b7449edf50d7d9321369
Daniel Nesterov <KOfTD@yandex.ru>
2026-03-14 21:27:50 +0300

    refactor: wrote a separate function to run a model

--- src/output_transforms.py
+++ src/output_transforms.py
@@ -1,4 +1,6 @@
 import time
+from datetime import timedelta
+from typing import NamedTuple
 
 import torch
 from torch.utils.data import DataLoader
@@ -6,38 +8,50 @@ from torch.utils.data import DataLoader
 from classification_network import ClassificationNetwork
 
 
+class RunResult(NamedTuple):
+    total: int
+    true_positives: int
+    false_negative: int
+    total_time: timedelta
+
+
+def _run_model(
+    data_loader: DataLoader[tuple[torch.Tensor, int]],
+    model: ClassificationNetwork,
+    device: torch.device,
+) -> RunResult:
+    n = 0
+    tp = 0
+    total_time = timedelta()
+    with torch.no_grad():
+        for images, labels in data_loader:  # pyright: ignore[reportAny]
+            images = images.requires_grad_().to(device)  # pyright: ignore[reportAny]
+            labels = labels.to(device)  # pyright: ignore[reportAny]
+            batch_start_time = timedelta(seconds=time.time())
+            outputs = model(images)  # pyright: ignore[reportAny]
+            batch_time = timedelta(seconds=time.time()) - batch_start_time
+            total_time += batch_time
+            _, predicted = torch.max(outputs.data, 1)  # pyright: ignore[reportAny]
+            n += labels.size(0)  # pyright: ignore[reportAny]
+            tp += (predicted == labels).sum()  # pyright: ignore[reportAny]
+
+    return RunResult(n, tp, n - tp, total_time)
+
+
 def get_accuracy(
     data_loader: DataLoader[tuple[torch.Tensor, int]],
     model: ClassificationNetwork,
     device: torch.device,
 ) -> float:
-    tp = 0
-    n = 0
-    with torch.no_grad():
-        for images, labels in data_loader:  # pyright: ignore[reportAny]
-            images = images.requires_grad_().to(device)  # pyright: ignore[reportAny]
-            labels = labels.to(device)  # pyright: ignore[reportAny]
-            outputs = model(images)  # pyright: ignore[reportAny]
-            _, predicted = torch.max(outputs.data, 1)  # pyright: ignore[reportAny]
-            n += labels.size(0)  # pyright: ignore[reportAny]
-            tp += (predicted == labels).sum()  # pyright: ignore[reportAny]
-    return float(tp / n)
+    result = _run_model(data_loader, model, device)
+    return result.true_positives / result.total
 
 
 def get_avg_time_per_image(
     data_loader: DataLoader[tuple[torch.Tensor, int]],
     model: ClassificationNetwork,
     device: torch.device,
-) -> float:
-    total_time = 0.0
-    n = 0
-    with torch.no_grad():
-        for images, labels in data_loader:
-            images = images.requires_grad_().to(device)
-            labels = labels.to(device)
-            batch_start_time = time.time()
-            _ = model(images)  # pyright: ignore[reportAny]
-            batch_time = time.time() - batch_start_time
-            total_time += batch_time
-            n += labels.size(0)
-    return float(total_time / n)
+) -> timedelta:
+    results = _run_model(data_loader, model, device)
+
+    return results.total_time / results.total
